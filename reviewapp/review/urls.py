5390ff644e09f34eb9004cc2e8aacef558404077
KailasHiwale <kailashiwale91@gmail.com>
2017-11-06 20:57:12 +0530

    reviewapp removed

--- reviewapp/review/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from review.views.review_view import ReviewViewset
-
-urlpatterns = [
-    url(r'^review/$', ReviewViewset.as_view(), name='review'),
-    url(r'^review/(?P<pk>[0-9]+)/$',
-        ReviewViewset.as_view(), name='review_pk'),
-]
